ff49e1263255389411315a3d66be51debc576ad2
Morteza-QN <mortezaghorbani3@gmail.com>
2023-07-14 21:55:13 +0330

    build src kts

---
+++ buildSrc/src/main/kotlin/AppDependencies.kt
@@ -0,0 +1,9 @@
+object AppDependencies {
+
+    //    const val  COMPOSE_UI = "androidx.compose.ui:ui:${BuildDependencyVersions.COMPOSE_VERSION}"
+    //    const val  COMPOSE_UI_TOOLING = "androidx.compose.ui:ui-tooling:${BuildDependencyVersions.COMPOSE_VERSION}"
+    //    const val  COMPOSE_UI_TOOLING_PREVIEW = "androidx.compose.ui:ui-tooling-preview:${BuildDependencyVersions.COMPOSE_VERSION}"
+    //    const val  COMPOSE_FOUNDATION = "androidx.compose.foundation:foundation:${BuildDependencyVersions.COMPOSE_VERSION}"
+    //    const val  COMPOSE_MATERIAL = "androidx.compose.material:material:${BuildDependencyVersions.COMPOSE_VERSION}"
+    //    const val  COMPOSE_ACTIVITY = "androidx.activity:activity-compose:${BuildDependencyVersions.COMPOSE_ACTIVITY_VERSION}"
+}
---
+++ buildSrc/src/main/kotlin/BuildDependencyVersions.kt
@@ -0,0 +1,4 @@
+object BuildDependencyVersions {
+    const val ANDROID_APP_LIBRARY_VERSION = "7.4.1"
+    const val ANDROID_MULTIPLATFORM_VERSION = "1.8.0"
+}
